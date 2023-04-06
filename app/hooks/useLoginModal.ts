85cecb84426edd87aab3003f5af24056f8bc13fc
Ratchawit Srithongdee <ratchawit.s@cdg.co.th>
2023-04-06 23:11:24 +0700

    add login and logout

--- app/hooks/useLoginModal.ts
+++ app/hooks/useLoginModal.ts
@@ -1,12 +1,12 @@
 import { create } from 'zustand'
 
-interface RegisterModalStore {
+interface LoginModalStore {
   isOpen: boolean
   onOpen: () => void
   onClose: () => void
 }
 
-const useRegisterModal = create<RegisterModalStore>((set) => ({
+const useRegisterModal = create<LoginModalStore>((set) => ({
   isOpen: false,
   onOpen: () => set({ isOpen: true }),
   onClose: () => set({ isOpen: false }),
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,10 @@ import { Nunito } from 'next/font/google'
 import './globals.css'
 import Navbar from './components/navbar/Navbar'
 import ClientOnly from './components/ClientOnly'
+import LoginModal from './components/modals/LoginModal'
 import RegisterModal from './components/modals/RegisterModal'
 import ToasterProvider from './providers/ToasterProvider'
-
+import getCurrentUser from './actions/getCurrentUser'
 export const metadata = {
   title: 'Airbnb',
   description: 'Airbnb clone',
@@ -14,18 +15,21 @@ const font = Nunito({
   subsets: ['latin'],
 })
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const currentUser = await getCurrentUser()
+
   return (
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
           <ToasterProvider />
+          <LoginModal />
           <RegisterModal />
-          <Navbar />
+          <Navbar currentUser={currentUser} />
         </ClientOnly>
         {children}
       </body>
